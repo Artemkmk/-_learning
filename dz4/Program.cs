51f519fa18001cd55f60bddb6d8e25aa4688e5cc
Artem <129191741+Artemkmk@users.noreply.github.com>
2024-02-02 11:16:35 +0300

    Add files via upload

---
+++ dz4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint chet = 0, nechet = 0;
+            int a, b, ChetSum=0, Nechetsum=0;
+            Console.WriteLine("введите первое число диапазона");
+            a =int.Parse(Console.ReadLine());
+            Console.WriteLine("введите второе число диапазона");
+            b = int.Parse(Console.ReadLine());
+
+            while (a!=b+1)
+            {
+                if (a % 2 == 0)
+                {
+                    ChetSum += a;
+                    chet++;
+                }
+                else
+                {
+                    Nechetsum += a;
+                    nechet++;
+                }
+                a++;
+            }
+            
+            Console.WriteLine("чётных чсел " + chet + " нечётных чисел " + nechet);
+            Console.WriteLine("сумма чётных чисел = " + ChetSum);
+            Console.WriteLine("сумма нечётных чисел = " + Nechetsum);
+            
+        }
+    }
+}
