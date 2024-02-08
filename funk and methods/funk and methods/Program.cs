4a1ca6240384a5480720b789a4e570668ddd4346
Artem <129191741+Artemkmk@users.noreply.github.com>
2024-02-08 17:29:33 +0300

    Add files via upload

---
+++ funk and methods/funk and methods/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace func_and_methods
+{
+    internal class Program
+    {
+
+        static string[] stroka(int value_1, string value_2)
+        {
+            Console.WriteLine("функция запущена");
+
+            string[] myArray = new string[value_1]; ;
+
+            for (int i = 0; i < value_1; i++)
+            {
+                myArray[i] = value_2;
+            }
+            Console.WriteLine(string.Join(", ", myArray)); // Выводим содержимое массива
+            return myArray;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("введите символ");
+            string b = (Console.ReadLine());
+
+            stroka(a, b);
+            PrintLine("#",10);
+
+            Console.ReadLine();
+        }
+
+        static void PrintLine(string value_1, int value_2) 
+        { 
+            for (int i = 0; i < value_2; i++)
+            {
+                Console.Write(value_1);
+            }
+        }
+            
+    }
+}
---
+++ funk and methods/funk and methods2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funk_and_methods2
+{
+    internal class Program
+    {
+
+        static int IndexOf(int value, int[] array)
+        {
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == value)
+                    return i;
+            }
+            return -1;
+        }
+
+        static int[] GetRandomArray(uint length, int minValue, int maxValue)
+        {
+            int[] myArray = new int[length];
+
+            Random random = new Random();
+
+            for (int i = 0;i < length; i++)
+            {
+                myArray[i]= random.Next(minValue, maxValue); 
+            }
+            return myArray;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] myArray = GetRandomArray(10, -20, 10);
+
+            Console.Write("введите индекс который хотите найти:\t");
+            int FoundIndex = int.Parse(Console.ReadLine());
+
+            int result = IndexOf(FoundIndex, myArray);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
